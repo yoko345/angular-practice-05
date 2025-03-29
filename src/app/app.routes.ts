fdce20bc73cd46006305a463b4b05f2057c4d73a
yoko345 <spblackhole89@gmail.com>
2025-03-29 10:25:47 +0900

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { InputComponent } from './input/input.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: InputComponent }];
